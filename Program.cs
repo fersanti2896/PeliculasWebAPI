0406a7a2f6e22bea1ecf1568fd780e5c7fecc133
Fernando Nicolás <fersanti2896@gmail.com>
2024-03-05 16:51:15 -0600

    Inicializando proyecto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+
+using SPeliculasAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+var serviceLogger = (ILogger<Startup>)app.Services.GetService(typeof(ILogger<Startup>));
+
+startup.Configure(app, app.Environment, serviceLogger);
+
+app.Run();
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SPeliculasAPI {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Peliculas API", Version = "v1", Description = "Web API de Películas", Contact = new OpenApiContact { Email = "fersanti2896@gmail.com" } });
+                
+                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.ApiKey,
+                //    Scheme = "Bearer",
+                //    BearerFormat = "JWT",
+                //    In = ParameterLocation.Header
+                //});
+
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //            }
+                //        },
+                //        new string[]{}
+                //    }
+                //});
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger) {
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Peliculas API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthorization();
+
+            app.UseEndpoints(end => {
+                end.MapControllers();
+            });
+        }
+    }
+}
